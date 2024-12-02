5e92bffddc41d1d13dd938d928287bf7846fda3a
不游 <71683364+mefengl@users.noreply.github.com>
2024-12-03 05:10:46 +0800

    Add host_permissions to manifest config

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   manifest: {
     default_locale: 'en',
     description: '__MSG_extension_description__',
+    host_permissions: [],
     name: '__MSG_extension_name__',
     permissions: [],
   },
